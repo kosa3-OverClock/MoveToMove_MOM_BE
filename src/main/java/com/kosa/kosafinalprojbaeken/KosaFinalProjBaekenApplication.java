6d7833efa533eb45fbc1f6973af69e8de426ddd5
백재우 <157946706+jaewoo9797@users.noreply.github.com>
2024-08-13 18:07:19 +0900

    프로젝트생성

---
+++ src/main/java/com/kosa/kosafinalprojbaeken/KosaFinalProjBaekenApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.kosafinalprojbaeken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosaFinalProjBaekenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KosaFinalProjBaekenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kosa/kosafinalprojbaeken/KosaFinalProjBaekenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosa.kosafinalprojbaeken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KosaFinalProjBaekenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
