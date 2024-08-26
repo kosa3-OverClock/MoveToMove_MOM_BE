5a418a1eb261b918e1edeff9477bbe52bd222735
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-08-26 23:36:31 +0900

    Feat: 회원가입 기능 추가
    Feat: 회원가입 기능 추가

--- src/main/java/com/kosa/kosafinalprojbackend/mybatis/mappers/member/MemberMapper.java
+++ src/main/java/com/kosa/kosafinalprojbackend/mybatis/mappers/member/MemberMapper.java
@@ -1,10 +1,17 @@
 package com.kosa.kosafinalprojbackend.mybatis.mappers.member;
 
+import com.kosa.kosafinalprojbackend.domains.member.model.form.SignUpForm;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface MemberMapper {
 
     // 이메일 중복 확인
-    boolean existsByEmail(String email);
+    boolean existsByMemberEmail(String email);
+    
+    // 닉네임 중복 확인
+    boolean existsByMemberNickName(String nickName);
+    
+    // 회원가입
+    void insertMember(SignUpForm signUpForm);
 }
