e35e7d495330c1f318f940703db26020c8bf9991
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-08-30 23:19:07 +0900

    Feat: 칸반 카드 담당자 조회
    Feat: 칸반 카드 담당자 조회

--- src/main/java/com/kosa/kosafinalprojbackend/domains/kanban/card/service/CardService.java
+++ src/main/java/com/kosa/kosafinalprojbackend/domains/kanban/card/service/CardService.java
@@ -1,10 +1,12 @@
 package com.kosa.kosafinalprojbackend.domains.kanban.card.service;
 
+import com.kosa.kosafinalprojbackend.domains.kanban.card.domain.dto.CardMemberDto;
 import com.kosa.kosafinalprojbackend.domains.kanban.card.domain.form.CardUpdateForm;
 import com.kosa.kosafinalprojbackend.domains.kanban.card.domain.form.CardUpdateMemberForm;
 import com.kosa.kosafinalprojbackend.global.error.exception.CustomBaseException;
 import com.kosa.kosafinalprojbackend.mybatis.mappers.kanbancard.KanbanCardMapper;
 import com.kosa.kosafinalprojbackend.mybatis.mappers.member.MemberMapper;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +21,23 @@ public class CardService {
   private final MemberMapper memberMapper;
   private final KanbanCardMapper kanbanCardMapper;
 
-  // 칸반 카드 사용자 수정
+  // 칸반 카드 담당자 조회
+  public List<CardMemberDto> selectKanbanCardMember(Long memberId, Long kanbanCardId) {
+
+    // 유저 아이디 확인
+    if (!memberMapper.existsByMemberId(memberId)) {
+      throw new CustomBaseException(NOT_FOUND_ID);
+    }
+
+    // 칸반 카드 확인
+    if (!kanbanCardMapper.existsByKanbanCardId(kanbanCardId)) {
+      throw new CustomBaseException(NOT_FOUND_KANBAN_CARD);
+    }
+
+    return kanbanCardMapper.selectKanbanCardMember(kanbanCardId);
+  }
+
+  // 칸반 카드 담당자 수정
   @Transactional
   public void updateKanbanCardMember(
       Long memberId, Long kanbanCardId, CardUpdateMemberForm kanbanCardTitleForm) {
