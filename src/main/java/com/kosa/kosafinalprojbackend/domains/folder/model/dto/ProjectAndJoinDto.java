cb5c2a999bcf4b1b1f60c9d54dc2b62c86608be8
badstar1004 <dmswjd4566@hanmail.net>
2024-08-31 16:56:35 +0900

    Feat: 폴더 생성, 수정, 삭제 로직 변경/생성
    - 폴더 생성 시 depth, seq 컬럼 들어가게 수정
    - 폴더 수정 시
       - 폴더명 수정
       - 폴더 위치 수정
    - 하위 폴더 조회
    - 폴더 삭제

--- src/main/java/com/kosa/kosafinalprojbackend/domains/folder/model/dto/ProjectAndJoinDto.java
+++ src/main/java/com/kosa/kosafinalprojbackend/domains/folder/model/dto/ProjectAndJoinDto.java
@@ -1,5 +1,6 @@
 package com.kosa.kosafinalprojbackend.domains.folder.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kosa.kosafinalprojbackend.domains.kanban.project.model.enums.ProjectLeaderYN;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
@@ -17,10 +18,14 @@ public class ProjectAndJoinDto {
   private String projectName;            // 프로젝트 명
   private String projectDescription;     // 프로젝트 설명
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
   private LocalDateTime startAt;         // 시작일시
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
   private LocalDateTime endAt;           // 종료일시
 
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
   private LocalDateTime createdAt;       // 생성일시
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
   private LocalDateTime deletedAt;       // 삭제일시
   
   private ProjectLeaderYN projectLeaderYN;// 프로젝트 팀장 여부
--- src/main/java/com/kosa/kosafinalprojbackend/global/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/kosa/kosafinalprojbackend/global/security/filter/JwtAuthenticationFilter.java
@@ -32,7 +32,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     String[] api = {
         "/api/members/login", "/api/members/sign-up",
-        "/oauth2/authorization/google", "/oauth2/authorization/kakao"
+        "/oauth2/authorization/google", "/oauth2/authorization/kakao", "/swagger-ui/**", "/v3/**"
     };
 
     String path = request.getRequestURI();
