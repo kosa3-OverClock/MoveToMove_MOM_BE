6630868d73c980ad37964e8ef2f2b0cd7cc8d75a
OhDongGeon <o.d.g@kakao.com>
2024-09-05 14:00:11 +0900

    Modify: 회원정보 수정 기능 수정
    Modify: 회원정보 수정 기능 수정

--- src/main/java/com/kosa/kosafinalprojbackend/global/error/exception/InternalServerException.java
+++ src/main/java/com/kosa/kosafinalprojbackend/global/error/exception/InternalServerException.java
@@ -1,10 +1,9 @@
 package com.kosa.kosafinalprojbackend.global.error.exception;
 
-
 import com.kosa.kosafinalprojbackend.global.error.errorCode.ResponseCode;
 
 public class InternalServerException extends CustomBaseException {
-    public InternalServerException(ResponseCode responseCode){
-        super(responseCode.getMessage(), responseCode);
-    }
+  public InternalServerException(ResponseCode responseCode) {
+    super(responseCode.getMessage(), responseCode);
+  }
 }
