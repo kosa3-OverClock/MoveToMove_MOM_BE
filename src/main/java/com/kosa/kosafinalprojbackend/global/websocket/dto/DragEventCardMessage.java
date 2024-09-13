69e315e2c4b62538f0960d9653c242359cb53163
백재우 <9712jw@gmail.com>
2024-09-13 15:03:44 +0900

    feat/bjw/kanban 칸반 카드 드래그 앤 드랍

---
+++ src/main/java/com/kosa/kosafinalprojbackend/global/websocket/dto/DragEventCardMessage.java
@@ -0,0 +1,13 @@
+package com.kosa.kosafinalprojbackend.global.websocket.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DragEventCardMessage {
+    private Long projectId;
+    private Long cardId;
+    private int oldIndex;
+    private int newIndex;
+}
