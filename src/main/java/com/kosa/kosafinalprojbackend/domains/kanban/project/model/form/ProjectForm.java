3b3e576820ee7c7d8eebb827481cd33a40e6b232
badstar1004 <dmswjd4566@hanmail.net>
2024-09-14 16:04:20 +0900

    Feat: 프로젝트 생성 로직 수정
    - 생성 로직 수정
    - 폴더 조회 시 컬럼 하나 추가

--- src/main/java/com/kosa/kosafinalprojbackend/domains/kanban/project/model/form/ProjectForm.java
+++ src/main/java/com/kosa/kosafinalprojbackend/domains/kanban/project/model/form/ProjectForm.java
@@ -2,14 +2,10 @@ package com.kosa.kosafinalprojbackend.domains.kanban.project.model.form;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.kosa.kosafinalprojbackend.domains.kanban.project.model.enums.ProjectLeaderYN;
-import com.kosa.kosafinalprojbackend.domains.member.model.dto.MemberDto;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.Map;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
@@ -20,11 +16,11 @@ public class ProjectForm {
     private String projectName;         // 프로젝트 명
     private String projectDescription;  // 프로젝트 설명
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime startAt;      // 시작일시
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate startAt;      // 시작일시
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime endAt;        // 종료일시
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private LocalDate endAt;        // 종료일시
 
     private Map<Long, ProjectLeaderYN> MemberDtoMap = new HashMap<>();  // 초대한 멤버 리스트
 
