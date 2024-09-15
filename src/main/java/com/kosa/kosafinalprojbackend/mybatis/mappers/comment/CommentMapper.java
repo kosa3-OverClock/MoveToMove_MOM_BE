50cf0ba71fd612336db05e493d5166ce763419dd
OhDongGeon <121795660+OhDongGeon@users.noreply.github.com>
2024-09-16 02:05:25 +0900

    Modify: 코멘트 수정
    Modify: 코멘트 수정

--- src/main/java/com/kosa/kosafinalprojbackend/mybatis/mappers/comment/CommentMapper.java
+++ src/main/java/com/kosa/kosafinalprojbackend/mybatis/mappers/comment/CommentMapper.java
@@ -1,5 +1,6 @@
 package com.kosa.kosafinalprojbackend.mybatis.mappers.comment;
 
+import com.kosa.kosafinalprojbackend.domains.kanban.card.domain.dto.CardCommentDto;
 import com.kosa.kosafinalprojbackend.domains.kanban.comment.domian.form.CommentForm;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
@@ -10,6 +11,9 @@ public interface CommentMapper {
   // 코멘트 작성자 확인
   boolean checkCommentWriter(@Param("memberId") Long memberId,
                              @Param("commentId") Long commentId);
+
+  // 코멘트 조회 (코멘트 아이디)
+  CardCommentDto selectCommentById(Long commentId);
 
   // 코멘트 내용 수정
   void updateComment(@Param("commentId") Long commentId,
