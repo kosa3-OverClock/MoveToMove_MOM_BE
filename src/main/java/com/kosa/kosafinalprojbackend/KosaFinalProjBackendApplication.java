0467422e4f0e672a7527839dfa701f404b21ece5
badstar1004 <dmswjd4566@hanmail.net>
2024-09-18 23:02:52 +0900

    Modify: 서브 모듈 수정, 서버시간 수정
    - 서버 시간 한국으로 수정

--- src/main/java/com/kosa/kosafinalprojbackend/KosaFinalProjBackendApplication.java
+++ src/main/java/com/kosa/kosafinalprojbackend/KosaFinalProjBackendApplication.java
@@ -1,5 +1,6 @@
 package com.kosa.kosafinalprojbackend;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -9,6 +10,8 @@ import org.springframework.scheduling.annotation.EnableAsync;
 public class KosaFinalProjBackendApplication {
 
     public static void main(String[] args) {
+        // 애플리케이션 시작 시 타임존 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(KosaFinalProjBackendApplication.class, args);
     }
 
